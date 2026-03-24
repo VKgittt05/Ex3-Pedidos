c5d9380f624080a4c7b79a7ee3cfc7092fcbb0bb
Victor Kauã <victorelezeu@gmail.com>
2026-03-24 00:18:11 -0300

    Controller

---
+++ Pedidos/src/main/java/com/Gerenciamento_Pedidos/Pedidos/controllers/PedidoController.java
@@ -0,0 +1,47 @@
+package com.Gerenciamento_Pedidos.Pedidos.controllers;
+
+
+import com.Gerenciamento_Pedidos.Pedidos.models.Pedido;
+import com.Gerenciamento_Pedidos.Pedidos.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+        import java.util.List;
+
+@RestController
+@RequestMapping("/pedidos")
+public class PedidoController {
+
+    @Autowired
+    private PedidoService service;
+
+    @PostMapping
+    public ResponseEntity<Pedido> criar(@RequestBody Pedido pedido) {
+        Pedido novoPedido = service.salvar(pedido);
+        return new ResponseEntity<>(novoPedido, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Pedido> listar() {
+        return service.listarTodos();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Pedido> buscar(@PathVariable Long id) {
+        return service.buscarPorId(id)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletar(@PathVariable Long id) {
+        if (service.buscarPorId(id).isPresent()) {
+            service.deletar(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+}
+
